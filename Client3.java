3c77cc7663d95bd3fdbd6c25a8e8f3e4d371a2e2
PratheekRebello <46563240+PratheekRebello@users.noreply.github.com>
2019-09-11 02:03:55 +0530

    Add files via upload

--- Client3.java
+++ Client3.java
@@ -3,7 +3,67 @@ import java.net.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Client {
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.security.MessageDigest; 
+
+import javax.crypto.Cipher;
+
+class Cryptography{
+
+    private static final String ALGORITHM = "RSA";
+
+    public static byte[] encrypt(byte[] publicKey, byte[] inputData)
+            throws Exception {
+        PublicKey key = KeyFactory.getInstance(ALGORITHM)
+                .generatePublic(new X509EncodedKeySpec(publicKey));
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+
+        byte[] encryptedBytes = cipher.doFinal(inputData);
+
+        return encryptedBytes;
+    }
+
+    public static byte[] decrypt(byte[] privateKey, byte[] inputData)
+            throws Exception {
+
+        PrivateKey key = KeyFactory.getInstance(ALGORITHM)
+                .generatePrivate(new PKCS8EncodedKeySpec(privateKey));
+
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key);
+
+        byte[] decryptedBytes = cipher.doFinal(inputData);
+
+        return decryptedBytes;
+    }
+
+    public static KeyPair generateKeyPair()
+            throws NoSuchAlgorithmException, NoSuchProviderException {
+
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance(ALGORITHM);
+
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+
+        // 512 is keysize
+        keyGen.initialize(512, random);
+
+        KeyPair generateKeyPair = keyGen.generateKeyPair();
+        return generateKeyPair;
+    }
+}
+
+public class Client3 {
 
     public Socket SendSocket = null; 
     public Socket ReceiveSocket = null; 
@@ -16,12 +76,25 @@ public class Client {
     BufferedReader inFromUser = null;
     public Thread t1, t2;
     boolean register = true;
+    byte[] publicKey;
+    byte[] privateKey;
     
-    public Client(String username, String ServerIP)
+    public Client3(String username, String ServerIP)
     {
         this.ServerIP = ServerIP;
         this.username = username;
         this.inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        try{
+        KeyPair generateKeyPair = Cryptography.generateKeyPair();
+        publicKey = generateKeyPair.getPublic().getEncoded();
+        privateKey = generateKeyPair.getPrivate().getEncoded();
+        }
+        catch(Exception e)
+        {
+            System.out.println("CryptoKey Error");
+        }
+        
+
         t1 = new Thread() {
 
             @Override
@@ -60,7 +133,7 @@ public class Client {
             {
                 SendSocket = new Socket(IP, 6000);
                 toSendServerStream = new DataOutputStream(SendSocket.getOutputStream());
-                inFromSendServer = new BufferedReader(new InputStreamReader(SendSocket.getInputStream())); 
+                inFromSendServer = new BufferedReader(new InputStreamReader(SendSocket.getInputStream()));
 
                 String username_packet = "REGISTER TOSEND " + username +"\n\n";
                 toSendServerStream.writeBytes(username_packet); 
@@ -140,9 +213,11 @@ public class Client {
                 ReceiveSocket = new Socket(ServerIP, 6100);
                 toReceiveServerStream = new DataOutputStream(ReceiveSocket.getOutputStream());
                 inFromReceiveServer = new BufferedReader(new InputStreamReader(ReceiveSocket.getInputStream())); 
-
-                String username_packet = "REGISTER TORECV " + username +"\n\n";
+                
+                String base64publicKey = java.util.Base64.getEncoder().encodeToString(this.publicKey);
+                String username_packet = "REGISTER TORECV " + username +"\n" +"Content-length: "+Integer.toString(base64publicKey.length())+"\n\n"+base64publicKey;
                 toReceiveServerStream.writeBytes(username_packet); 
+                //toReceiveServerStream.write(publicKey,0,publicKey.length);
                 String response = inFromReceiveServer.readLine();
                 String newline = inFromReceiveServer.readLine();
 
@@ -342,10 +417,8 @@ public class Client {
                     System.out.println("Message should be in format @[username] [message]");
                     continue;
                 }
-                String output = "SEND " + to_username +"\nContent-length: "+array[1].length()
-                                    +"\n\n"+array[1];
-                //System.out.println(output);
-                //System.out.print(array[1]);
+
+                String output = "FETCHKEY " + to_username+"\n\n";
                 
                 while(true){
                     try{
@@ -357,11 +430,79 @@ public class Client {
                         openSendSocket(this.ServerIP);
                     }
                 }
+
+                input = inFromSendServer.readLine();
+                boolean correct = true;
+
+                int length = 0;
+
+                Pattern pattern = Pattern.compile("FETCHEDKEY (.*?)$");
+                Matcher matcher = pattern.matcher(input);
+                if (matcher.find())
+                    to_username = matcher.group(1);
+                else
+                    correct = false;
+
+                input = inFromSendServer.readLine();
+                pattern = Pattern.compile("Content-length: (.*?)$");
+                matcher = pattern.matcher(input);
+                if (matcher.find())
+                    length = Integer.parseInt(matcher.group(1));
+                else
+                    correct = false;
+            // System.out.println("Message Length :"+Integer.toString(length));
+                input = inFromSendServer.readLine();
+
+                if(correct == false)
+                {
+                    output = "ERROR 103 Header incomplete\n\n";
+                    System.out.println("Error with Server");
+                    toSendServerStream.writeBytes(output);
+                    SendSocket.close();
+                    openSendSocket(this.ServerIP);
+                    continue;
+                }
+
+                char[] msg_buf= new char[length];
+                int contentLength = inFromSendServer.read(msg_buf,0,length);
+                String receiverPublicKeyString = new String(msg_buf);
+
+                byte[] receiverPublicKey = java.util.Base64.getDecoder().decode(receiverPublicKeyString);
+
+                //byte[] encryptedData = 
+
+
+                String encryptedData = java.util.Base64.getEncoder().encodeToString(Cryptography.encrypt(receiverPublicKey,array[1].getBytes()));
+                 
+
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] shaBytes = md.digest(Cryptography.encrypt(receiverPublicKey,array[1].getBytes()));
+                String signature = java.util.Base64.getEncoder().encodeToString(Cryptography.encrypt(this.privateKey,shaBytes));
+
+
+                output = "SEND " + to_username +"\nContent-length: "+Integer.toString(encryptedData.length())
+                                    +"\nSignature: " + signature 
+                                    +"\n\n" + encryptedData;
+                                    //+array[1];
+                //System.out.println(output);
+                //System.out.print(array[1]);
+                
+                while(true){
+                    try{
+                        toSendServerStream.writeBytes(output);
+                        //toSendServerStream.write(encryptedData,0,encryptedData.length);
+                        break; 
+                    }
+                    catch(Exception e)
+                    {
+                        openSendSocket(this.ServerIP);
+                    }
+                }
             //was throwing error since username was null and username.equals was called
                 String response = inFromSendServer.readLine();
                 String newline = inFromSendServer.readLine();
-                Pattern pattern = Pattern.compile("SENT (.*?)$");
-                Matcher matcher = pattern.matcher(response);
+                pattern = Pattern.compile("SENT (.*?)$");
+                matcher = pattern.matcher(response);
                 if (matcher.find())
                 {
                     if(to_username.equals(matcher.group(1))){
@@ -428,6 +569,17 @@ public class Client {
                 length = Integer.parseInt(matcher.group(1));
             else
                 correct = false;
+
+            String signature="";
+            input = inFromReceiveServer.readLine();
+            pattern = Pattern.compile("Signature: (.*?)$");
+            matcher = pattern.matcher(input);
+            if (matcher.find())
+                signature = (matcher.group(1));
+            else
+                correct = false;
+            byte[] sigbyte = java.util.Base64.getDecoder().decode(signature);
+
             // System.out.println("Message Length :"+Integer.toString(length));
             input = inFromReceiveServer.readLine();
 
@@ -440,14 +592,87 @@ public class Client {
                 continue;
             }
 
-            char [] msg_buf = new char[length];
+            char[] msg_buf = new char[length];
             int contentLength = inFromReceiveServer.read(msg_buf,0,length);
-            String message = new String(msg_buf);
+            String messageString = new String(msg_buf);
             
             // for(int i =0;i<length;i++)
                 // message = message +inFromReceiveServer.read();
+            //String = encryptedData
+            byte[] decryptedData = Cryptography.decrypt(this.privateKey, java.util.Base64.getDecoder().decode(messageString));
+            String message = new String(decryptedData);
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] shaBytes = md.digest(java.util.Base64.getDecoder().decode(messageString));
 
-            String output = "RECEIVED "+fromusername+"\n\n";
+            String output = "FETCHKEY " + fromusername+"\n\n";
+                
+            while(true){
+                try{
+                    toReceiveServerStream.writeBytes(output);
+                    break; 
+                }
+                catch(Exception e)
+                {
+                    openReceiveSocket(this.ServerIP);
+                }
+            }
+
+            input = inFromReceiveServer.readLine();
+            correct = true;
+
+            length = 0;
+
+            pattern = Pattern.compile("FETCHEDKEY (.*?)$");
+            matcher = pattern.matcher(input);
+            if (matcher.find())
+                fromusername = matcher.group(1);
+            else
+                correct = false;
+
+            input = inFromReceiveServer.readLine();
+            pattern = Pattern.compile("Content-length: (.*?)$");
+            matcher = pattern.matcher(input);
+            if (matcher.find())
+                length = Integer.parseInt(matcher.group(1));
+            else
+                correct = false;
+        // System.out.println("Message Length :"+Integer.toString(length));
+            input = inFromReceiveServer.readLine();
+
+            if(correct == false)
+            {
+                output = "ERROR 103 Header incomplete\n\n";
+                System.out.println("Error with Server");
+                toReceiveServerStream.writeBytes(output);
+                ReceiveSocket.close();
+                openReceiveSocket(this.ServerIP);
+                continue;
+            }
+
+            msg_buf= new char[length];
+            contentLength = inFromReceiveServer.read(msg_buf,0,length);
+            String senderPublicKeyString = new String(msg_buf);
+
+            byte[] senderPublicKey = java.util.Base64.getDecoder().decode(senderPublicKeyString);
+            byte[] KpubHash = Cryptography.decrypt(senderPublicKey,sigbyte);
+
+            System.out.println(KpubHash);
+            System.out.println(shaBytes);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            output = "RECEIVED "+fromusername+"\n\n";
             toReceiveServerStream.writeBytes(output);
 
             System.out.println("From "+fromusername+ ": "+message);
