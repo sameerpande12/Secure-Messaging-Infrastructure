8739adc4efe668af2f2d71d81592f6fa161f3ab5
PratheekRebello <46563240+PratheekRebello@users.noreply.github.com>
2019-09-10 16:58:03 +0530

    Add files via upload

--- Client.java
+++ Client.java
@@ -18,19 +18,19 @@ public class Client {
     
     public Client(String username, String ServerIP)
     {
-        ServerIP = ServerIP;
-        username = username;
-        inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        this.ServerIP = ServerIP;
+        this.username = username;
+        this.inFromUser = new BufferedReader(new InputStreamReader(System.in));
     }
     public void InitialiseSend()
     {
         boolean a = openSendSocket(ServerIP);
-        a = registerToSend(username);
+        //a = registerToSend(this.username);
     }
     public void InitialiseReceive()
     {
         boolean a = openReceiveSocket(ServerIP);
-        a = registerToReceive(username);
+        //a = registerToReceive(this.username);
     }
 
     public boolean openSendSocket(String IP)
@@ -41,19 +41,41 @@ public class Client {
                 SendSocket = new Socket(IP, 6000);
                 toSendServerStream = new DataOutputStream(SendSocket.getOutputStream());
                 inFromSendServer = new BufferedReader(new InputStreamReader(SendSocket.getInputStream())); 
-                return true;
+
+                String username_packet = "REGISTER TOSEND " + username +"\n\n";
+                toSendServerStream.writeBytes(username_packet); 
+                String response = inFromSendServer.readLine();
+                String newline = inFromSendServer.readLine();
+                Pattern pattern = Pattern.compile("REGISTERED TOSEND (.*?)$");
+                Matcher matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    if(username.equals(matcher.group(1))){
+                        System.out.println("You have been registered to send!");
+                        return true;
+                    }
+                }
+                pattern = Pattern.compile("ERROR 100 Malformed username");
+                matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    System.out.println("Malformed Username");
+                    return false;
+                }    
+                return false;
             } catch (UnknownHostException e) {
-                System.err.println("Don't know about host: hostname");
+                System.err.println("Don't know about host: "+ this.ServerIP);
             } catch (IOException e) {
-                System.err.println("Couldn't get I/O for the connection to: hostname");
+                System.err.println("Server is not Online. Trying to Establish Connection.");
             }
         }
     }
+    /*
     public boolean registerToSend(String username)
     {
+        while(true){
         if(SendSocket == null)
             openSendSocket(ServerIP);
-        while(true){
             try
             {
                 //System.out.print("Please Enter Your Username: ");
@@ -87,7 +109,7 @@ public class Client {
                 //System.err.println("Please Try Again");
             }
         }
-    }
+    }*/
 
     public boolean openReceiveSocket(String ServerIP)
     {
@@ -98,20 +120,44 @@ public class Client {
                 ReceiveSocket = new Socket(ServerIP, 6100);
                 toReceiveServerStream = new DataOutputStream(ReceiveSocket.getOutputStream());
                 inFromReceiveServer = new BufferedReader(new InputStreamReader(ReceiveSocket.getInputStream())); 
-                return true;
+
+                String username_packet = "REGISTER TORECV " + username +"\n\n";
+                toReceiveServerStream.writeBytes(username_packet); 
+                String response = inFromReceiveServer.readLine();
+                String newline = inFromReceiveServer.readLine();
+
+                Pattern pattern = Pattern.compile("REGISTERED TORECV (.*?)$");
+                Matcher matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    if(username.equals(matcher.group(1))){
+                        System.out.println("You have been registered to receive!");
+                        return true;
+                    }
+                }
+                pattern = Pattern.compile("ERROR 100 Malformed username");
+                matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    System.out.println("Malformed Username");
+                    return false;
+                }
+
+                return false;
             } catch (UnknownHostException e) {
-                System.err.println("Don't know about host: hostname");
+                System.err.println("Don't know about host: "+ ServerIP);
             } catch (IOException e) {
-                System.err.println("Couldn't get I/O for the connection to: hostname");
+                System.err.println("Server is not Online. Trying to Establish Connection.");
             }
         }
     }
-
+    /*
     public boolean registerToReceive(String username)
     {
+        while(true){
+            
         if(ReceiveSocket == null)
             openReceiveSocket(ServerIP);
-        while(true){
             try
             {
                 
@@ -140,24 +186,29 @@ public class Client {
                 }    
                 //System.out.println("Please Try Again");
             }
+            
             catch (IOException e)
             {
                 System.err.println("Server Not Online");
             }
         }
     }
-
+    
     public void send()
     {
         while(true){
 
             try{
+
             String input = inFromUser.readLine();
             if(input.charAt(0) != '@')
+            {
+                System.out.println("Message should be in format @[username] [message]");
                 continue;
+            }
             String[] array = input.substring(1).split(" ",2);
             if(array.length <2){
-                System.out.println("Wrong Format!!\n");
+                System.out.println("Message should be in format @[username] [message]");
                 continue;
             }
             username = array[0];
@@ -202,6 +253,81 @@ public class Client {
                 }
 
         }
+    }*/
+
+    public void send()
+    {
+        while(true){
+
+            try{
+                
+                String input = inFromUser.readLine();
+                
+                if(input.charAt(0) != '@')
+                {
+                    System.out.println("Message should be in format @[username] [message]");
+                    continue;
+                }
+                String[] array = input.substring(1).split(" ",2);
+                if(array.length <2){
+                    System.out.println("Message should be in format @[username] [message]");
+                    continue;
+                }
+                username = array[0];
+                if(username.equals("")){
+                    System.out.println("Message should be in format @[username] [message]");
+                    continue;
+                }
+                String output = "SEND " + array[0] +"\nContent-length: "+array[1].length()
+                                    +"\n\n"+array[1];
+                //System.out.println(output);
+                //System.out.print(array[1]);
+                
+                while(true){
+                    try{
+                        toSendServerStream.writeBytes(output);
+                        break; 
+                    }
+                    catch(Exception e)
+                    {
+                        openSendSocket(this.ServerIP);
+                }
+                }
+            //was throwing error since username was null and username.equals was called
+                String response = inFromSendServer.readLine();
+                String newline = inFromSendServer.readLine();
+                Pattern pattern = Pattern.compile("SENT (.*?)$");
+                Matcher matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    if(username.equals(matcher.group(1))){
+                        System.out.println("Message Sent!");
+                        continue;
+                    }
+                }
+                pattern = Pattern.compile("ERROR 102 Unable to send$");
+                matcher = pattern.matcher(response);
+                if (matcher.find()){
+                    System.out.println("Unable to Send!");
+                    continue;
+                }
+                pattern = Pattern.compile("ERROR 103  Header incomplete");
+                matcher = pattern.matcher(response);
+                if (matcher.find())
+                {
+                    System.out.println("Header Incomplete!");
+                    boolean a = openSendSocket(this.ServerIP);
+                }
+                
+            }
+            catch(Exception e)
+            {
+                //System.out.println("Error");
+                openSendSocket(this.ServerIP);
+                //throw new NullPointerException();  
+            }
+
+        }
     }
 
     public void receive()
@@ -238,6 +364,7 @@ public class Client {
                 String output = "ERROR 103 Header incomplete\n\n";
                 toReceiveServerStream.writeBytes(output);
                 ReceiveSocket.close();
+                openReceiveSocket(this.ServerIP);
                 continue;
             }
 
@@ -255,23 +382,34 @@ public class Client {
             }
             catch (IOException e) {
                 System.err.println("Couldn't get I/O for the connection to: hostname");
-                InitialiseReceive();
+                //InitialiseReceive();
             }
             catch(Exception e)
             {
-                System.out.println("WrongLength");
+                //System.out.println("No " +ReceiveSocket);
+                if(inFromReceiveServer == null){
+                    openReceiveSocket(this.ServerIP);
+                    continue;
+                }
+                //System.out.println("WrongLength");
                 String output = "ERROR 103 Header incomplete\n\n";
-                try{
-                toReceiveServerStream.writeBytes(output);
-                ReceiveSocket.close();
+                try
+                {
+                    toReceiveServerStream.writeBytes(output);
+                    ReceiveSocket.close();
+                    openReceiveSocket(this.ServerIP);
+                    continue;
                 }
                 catch(Exception r)
                 {
-                    System.out.println("No connection");
+                    openReceiveSocket(this.ServerIP);
+                    continue;
+                    //throw new NullPointerException();
+                    //System.out.println("No connection");
                 }
                 
-                InitialiseReceive();
-                continue;
+                //InitialiseReceive();
+                
             }
 
             
@@ -282,20 +420,20 @@ public class Client {
 
     public static void main(String[] args) {
     
-        Client client = new Client(args[0],"localhost");
+        Client client = new Client(args[0],args[1]);
 
         boolean a = client.openSendSocket(args[1]);
         if(a == false)
             return;
-        a = client.registerToSend(args[0]);
-        if(a == false)
-            return;
+        //a = client.registerToSend(args[0]);
+        //if(a == false)
+        //   return;
         a = client.openReceiveSocket(args[1]);
         if(a == false)
             return;
-        a = client.registerToReceive(args[0]);
-        if(a == false)
-            return;
+        //a = client.registerToReceive(args[0]);
+        //if(a == false)
+        //    return;
         System.out.println("\nYou can now start sending messages!\n");
         //client.send();
         //client.receive();
@@ -312,8 +450,11 @@ public class Client {
                 client.receive();
             }
         };
-        t1.start();
-        t2.start();
+
+        
+            t1.start();
+            t2.start();
+        
         
         
         
